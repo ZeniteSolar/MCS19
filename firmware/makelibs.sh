435cd8af890f943d145418df696c4ca1b578a883
João Antônio Cardoso <joao.maker@gmail.com>
2020-01-19 23:42:55 -0300

    can_ids removed, makelibs updated

--- firmware/makelibs.sh
+++ firmware/makelibs.sh
@@ -4,4 +4,10 @@ git submodule update
 cd src/
 make clean
 make lib
-cd ../../
+cd ../../../
+cd lib/DISPLAYS/SSD1306_AVR
+make clean
+make
+cd src/
+wget https://raw.githubusercontent.com/ZeniteSolar/CAN_IDS/master/can_ids.h
+cd ..
